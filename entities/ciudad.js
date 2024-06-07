abc07402710a21ea2318693b37df1f4e68d3d1d8
KudoaGamer <81930144+Kudoa1@users.noreply.github.com>
2024-06-07 12:45:36 -0300

    6, agregar al 5 y 6 el TEST

--- entities/ciudad.js
+++ entities/ciudad.js
@@ -1,4 +1,16 @@
 export class Ciudad{
     nombre;
-    aeropuertos;
+    aeropuertos=[];
+
+    constructor(nombre){
+        this.nombre=nombre;
+    }
+
+    agregarAeropuerto(aeropuerto){
+        this.aeropuertos.push(aeropuerto);
+    }
+
+    cantAeropuertos(){
+        return this.aeropuertos.lenght;
+    }
 }
