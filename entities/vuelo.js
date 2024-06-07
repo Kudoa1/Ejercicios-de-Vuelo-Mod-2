2b5d6f0b7c921fda21d9fd1abb4d822beb6b3724
KudoaGamer <81930144+Kudoa1@users.noreply.github.com>
2024-06-07 10:19:48 -0300

    Agregado de metodos a clase Vuelo
    Metodo 1:
            agregarPasajero(pasajero){
             this.pasajeros.push(pasajero);
        }
    Metodo2:
        capacidadOcupadaPorPasajeros(){
            //porcentaje (regla de tres)
            return (this.pasajeros.length*100) /this.avion.cantAsientos;
        }

--- entities/vuelo.js
+++ entities/vuelo.js
@@ -6,4 +6,14 @@ export class Vuelo{
     AeropuertoPartida;
     AeropuertoSalida;
     AeropuertoDestino;
+
+    //ya que tiene una colección de pasajeros, solamente esta clase los deberia manipular
+    agregarPasajero(pasajero){
+        this.pasajeros.push(pasajero);
+    }
+
+    capacidadOcupadaPorPasajeros(){
+        //porcentaje (regla de tres)
+        return (this.pasajeros.length*100) /this.avion.cantAsientos;
+    }
 }
