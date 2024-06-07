a716f6c7d402c77909135bad95a958c6712158a6
KudoaGamer <81930144+Kudoa1@users.noreply.github.com>
2024-06-07 11:21:38 -0300

    Update Class Viaje (punto 2)
     -> Nuevos Metodos (punto 2)

--- entities/viaje.js
+++ entities/viaje.js
@@ -1,4 +1,26 @@
 export class Viaje{
     vuelos=[];
     escalas=[];
+
+    agregarVuelo(vuelo){
+        this.vuelos.push(vuelo);
+    }
+    agregarEscala(escala){
+        this.escalas.push(escala);
+    }
+
+    duracionTotalVuelos(){
+        let duracion=0;
+        for(let i=0;i<this.vuelos.length;i++){duracion+=this.vuelos[i].duracionAproxEnMins};
+        return duracion;
+    }
+    duracionTotalEscalas(){
+        let duracion=0;
+        //lo mismo que hacer un FOR
+        this.escalas.forEach(escala => {duracion+=escala.duracionAproxEnMins});
+        return duracion;
+    }
+    duracionTotalAproxEnMins(){
+        return this.duracionTotalVuelos() + this.duracionTotalEscalas()
+    }
 }
