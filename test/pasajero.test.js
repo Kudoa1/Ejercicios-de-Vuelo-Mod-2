a4a4504ad57dd78ee507b8d8433de80591027730
VOrtigosa <ortigosa.valen@gmail.com>
2024-06-14 09:21:49 -0300

    test 8

--- test/pasajero.test.js
+++ test/pasajero.test.js
@@ -18,22 +18,23 @@ test("Le paso un pasajero con 3 vuelos y me tiene que devolver 3", ()=>{
     expect(pasajero.cantVuelos()).toBe(3);
 }); 
 
-// test("Ingreso un pasajero, dos vuelos, una ciudad y me tiene que decir que la visito 2 veces", () =>{
-//     let aeropuerto = new Aeropuerto();
+//8
+ test("Ingreso un pasajero, dos vuelos, una ciudad y me tiene que decir que la visito 2 veces", () =>{
+     let aeropuerto = new Aeropuerto();
 
-//     let vuelo1 = new Vuelo(null);
-//     let vuelo2 = new Vuelo(null);
+     let vuelo1 = new Vuelo(null);
+     let vuelo2 = new Vuelo(null);
 
-//     vuelo1.aeropuertoSalida = aeropuerto;
-//     vuelo2.aeropuertoSalida = aeropuerto;
+     vuelo1.aeropuertoSalida = aeropuerto;
+     vuelo2.aeropuertoSalida = aeropuerto;
 
-//     let ciudad = new Ciudad(null);
-//     ciudad.agregarAeropuerto(aeropuerto);
+     let ciudad = new Ciudad(null);
+     ciudad.agregarAeropuerto(aeropuerto);
 
-//     let pasajero = new Pasajero();
+     let pasajero = new Pasajero();
 
-//     pasajero.agregarVuelo(vuelo1);
-//     pasajero.agregarVuelo(vuelo2);
+     pasajero.agregarVuelo(vuelo1);
+     pasajero.agregarVuelo(vuelo2);
 
-//     expect(pasajero.cantVecesQueVisitaste(ciudad)).toBe(2);
-// });
+     expect(pasajero.cantVecesQueVisitaste(ciudad)).toBe(2);
+ });
